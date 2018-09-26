559f930fa989a3988eb105b3f72f7d4da4be42e3
Junwei Han <hanjunwei1981@163.com>
2018-09-26 13:20:06 +0000

    version 1.1

--- R/findSigGenes.r
+++ R/findSigGenes.r
@@ -1,14 +1,14 @@
 
 
-findSigGenes <- function(Expr, Label, Method = "tTest", Directed = TRUE, FdrCut = 0.01, FDCut = 1, SAMfdr = 0.20){
+findSigGenes <- function(Expr, Label, Method = "tTest", Directed = TRUE, FdrCut = 0.01, FDCut = 1){
 ## for a given Expression profile, use statistic approach to evaluate the significance of difference between case and control samples;
 ## Expr: a data frame, the expression profile to be dealed with, the rownames should be the IDs of genes;
-## Method: a string, should be one of the "tTest", "foldChange" or "SAM";
+## Method: a string, should be one of the "tTest" or "foldChange";
 ## Label: a vector of 0/1s, indicating the class of samples in the expression profile, 0 represents case,1 represents control;
 ## Directed: logical, if the the up or down regulated set should be distinguished;
 ## FdrCut: the fdr cutoff for T test;
 ## FDCut: the cutoff for fold change;
-## SAMfdr: the parameter fdr.output used in SAM;
+
         if(Method == "tTest"){
                 Tscore <- calT(Expr, Label);
                 P_Val <- pt(abs(Tscore$T), lower.tail = FALSE, df = Tscore$df);
@@ -40,19 +40,6 @@ findSigGenes <- function(Expr, Label, Method = "tTest", Directed = TRUE, FdrCut 
                         return(SigGenes);
                 }
 
-        }else if(Method == "SAM"){
-                        Label[which(Label == 0)] <- 2;
-                        Result <- SAM(x = Expr, y = Label, resp.type = "Two class unpaired", geneid = rownames(Expr), fdr.output = SAMfdr);
-                        UpGeneSet <- Result$siggenes.table$genes.up[,2];
-                        LoGeneSet <- Result$siggenes.table$genes.lo[,2];
-                        if(Directed == TRUE){
-                                SigGenes <- list(UpGeneSet, LoGeneSet);
-                                names(SigGenes) <- c("Up", "Low");
-                                return(SigGenes);
-                        }else{
-                                SigGenes <- c(UpGeneSet, LoGeneSet);
-                                return(SigGenes);
-                        }
         }
 }
 
